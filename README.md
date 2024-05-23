bf81468bf94751b7aeb69bc16edb3ff373874402
iamtanim0195 <sahadatmd195@gmail.com>
2024-05-24 05:58:46 +0600

    Create basic project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# recipes-hub
+# recipe-hub
+# client side packages
+1. npm i react-router-dom
+2. npm i react-helmet-async
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { HelmetProvider } from 'react-helmet-async';
+import { router } from './routes/Routes';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+        <HelmetProvider>
+          <Toaster />
+          <RouterProvider router={router} />
+        </HelmetProvider>
+
   </React.StrictMode>,
-)
+);
